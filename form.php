4b1a8f963fda5153712ad62c51d176ebbc675f91
Ember <Hexagon@leafs.home>
2017-11-28 21:44:58 +0000

    waaaaa

--- form.php
+++ form.php
@@ -3,10 +3,10 @@
 $to = "emberashdown@gmail.com";
 $name = $_POST["name"];
 $email = $_POST["email"];
-$message = $_POST["message"];
+$message = "Message from " .$email. " \n" .$_POST["message"];
 $subject = "Message from" .$name;
 mail($to, $subject, $message);
 
-echo($name);
-echo($email);
-echo($message);
+
+
+echo($name."\n".$email."\n".$message);
