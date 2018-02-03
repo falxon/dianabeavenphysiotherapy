fee306edccdb6739323c852575b3f9e3da7834d4
Ember <falconsflight@riseup.net>
2018-02-03 20:33:24 +0000

    Hopefully fixed the email problem

--- index.php
+++ index.php
@@ -245,19 +245,13 @@ $currentpage = $_SERVER['REQUEST_URI'];
 
 if($currentpage=="/home" || $currentpage == "/"){
 	if(isset($_POST["name"])&& isset($_POST["email"])&& isset($_POST["message"])){
-		$to = "email1";
+		$to = "contact@dianabeaven.com";
 		$name = $_POST["name"];
 		$email = $_POST["email"];
 		$headers = 'From: contact@dianabeavenphysiotherapy.com' . "\r\n" .
     		"Reply-To:".$_POST["email"];
 		$message = "Message from " .$email. "\n" .$_POST["message"];
 		$subject = "Message from " .$name;
-		mail($to, $subject, $message, $headers);
-		$to = "email2";
-		mail($to, $subject, $message, $headers);
-		$to = "email3";
-		mail($to, $subject, $message, $headers);
-		$to = "email3";
 		mail($to, $subject, $message, $headers);
 		$home["email_sent"][0]["type"] = "alert";
 		$home["email_sent"][0]["message"] = "Your message has been sent. Diana Beaven Physiotherapist will get back to you shortly.";
