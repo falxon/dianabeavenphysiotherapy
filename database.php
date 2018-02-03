88deba714e5fa814eae9acc0ea79ad7afdde9b09
Ember <falconsflight@riseup.net>
2018-02-03 17:21:03 +0000

    thing

--- database.php
+++ database.php
@@ -3,7 +3,7 @@ require "vendor/redbean/rb.php";
 require "secure.php";
 
 # Database
-R::setup('mysql:host=localhost;dbname=physio',
+R::setup('mysql:dbname=physio',
         $database_username, $database_password);
 
 
