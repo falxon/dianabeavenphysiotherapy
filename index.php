d9a165f761553af80a28a5a0b47d2492dd1d3819
Ember <falconsflight@riseup.net>
2018-02-04 15:52:46 +0000

    If it doesn't break properly this time I'm going to go to your data banks with a large axe and give you a reprogramming you'll never forget.

--- index.php
+++ index.php
@@ -244,8 +244,7 @@ $error["card"][0]["card_text"] = "The page you are looking for is currently unde
 $currentpage = $_SERVER['REQUEST_URI'];
 $floop = true;
 if($currentpage=="/home" || $currentpage == "/"){
-	//if(isset($_POST["name"])&& isset($_POST["email"])&& isset($_POST["message"])){
-    if($floop == false){
+  if(isset($_POST["name"])&& isset($_POST["email"])&& isset($_POST["message"])){
     $to = "contact@dianabeavenphysiotherapy.com";
 		$name = $_POST["name"];
 		$email = $_POST["email"];
@@ -254,7 +253,8 @@ if($currentpage=="/home" || $currentpage == "/"){
         "\r\nContent-Type: text/html; charset=UTF-8\r\n";
 		$message = "Message from " .$email. "\n" .$_POST["message"];
 		$subject = "Message from " .$name;
-		if(mail($to, $subject, $message, $headers)){
+		//if(mail($to, $subject, $message, $headers)){
+    if($floop == false){
   		$home["email_sent"][0]["type"] = "alert";
   		$home["email_sent"][0]["message"] = "Your message has been sent. Diana Beaven Physiotherapist will get back to you shortly.";
     }else{
