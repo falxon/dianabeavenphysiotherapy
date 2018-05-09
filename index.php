fdf47261767f234ce1bf764725246814ef2bd7aa
MandyCaps <34073187+MandyCaps@users.noreply.github.com>
2018-05-09 17:28:25 +0100

    added mandatory https redirection

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+// MANDATORY HTTPS REDIRECTION
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+
 require "vendor/autoload.php";
 require "vendor/redbean/rb.php";
 require "secure.php";
